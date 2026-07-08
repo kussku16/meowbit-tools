2d89b939b9397313803cad4b97dd2806e5590e8a
Markus <kussku@gmail.com>
2026-07-08 19:38:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -55,6 +55,42 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             temperatureSprite.setScale(1.3, ScaleAnchor.Middle)
             optionOpen = true
             menuOpen = false
+        } else {
+            if (cursorPosition == 1 && optionOpen == false) {
+                scene.setBackgroundColor(7)
+                sprites.destroyAllSpritesOfKind(SpriteKind.Text)
+                lightSprite = textsprite.create("Light level: " + light2 + " units", 0, 8)
+                lightSprite.setPosition(78, 60)
+                lightSprite.setScale(1.2, ScaleAnchor.Middle)
+                optionOpen = true
+                menuOpen = false
+            } else {
+                if (cursorPosition == 2 && optionOpen == false) {
+                    scene.setBackgroundColor(7)
+                    sprites.destroyAllSpritesOfKind(SpriteKind.Text)
+                    acceleration = textsprite.create("Acceleration", 0, 8)
+                    acceleration.setPosition(50, 20)
+                    acceleration.setScale(1.2, ScaleAnchor.Middle)
+                    accelerationX = textsprite.create("X" + controller.acceleration(ControllerDimension.X), 0, 8)
+                    accelerationX.setPosition(30, 40)
+                    accelerationY = textsprite.create("Y" + controller.acceleration(ControllerDimension.Y), 0, 8)
+                    accelerationY.setPosition(30, 50)
+                    accelerationZ = textsprite.create("Z" + controller.acceleration(ControllerDimension.Z), 0, 8)
+                    accelerationZ.setPosition(30, 60)
+                    optionOpen = true
+                    menuOpen = false
+                } else {
+                    if (cursorPosition == 3 && optionOpen == false) {
+                        scene.setBackgroundColor(7)
+                        sprites.destroyAllSpritesOfKind(SpriteKind.Text)
+                        stopWatch.setPosition(50, 20)
+                        optionOpen = true
+                        menuOpen = false
+                    } else {
+                    	
+                    }
+                }
+            }
         }
     }
 })
@@ -64,35 +100,36 @@ function menu2 () {
     temperatureSprite = textsprite.create("Temperature: " + temperature + " C", 0, 8)
     lightSprite = textsprite.create("Light level: " + light2 + " units", 0, 8)
     menu = textsprite.create("MeowTools", 0, 8)
-    tilt = textsprite.create("Tilt", 0, 8)
+    acceleration = textsprite.create("Acceleration", 0, 8)
     temperatureSprite.setText("Temperature")
     lightSprite.setText("Light")
     stopWatch = textsprite.create("Stopwatch", 0, 8)
     timer = textsprite.create("Timer", 0, 8)
     dice = textsprite.create("Dice", 0, 8)
-    acceleration = textsprite.create("Acceleration", 0, 8)
     cursorSprite = textsprite.create(">>", 0, 2)
     menu.setScale(1.5, ScaleAnchor.Middle)
     menu.setPosition(45, 8)
     temperatureSprite.setPosition(53, 24)
     lightSprite.setPosition(35, 37)
-    tilt.setPosition(32, 50)
+    acceleration.setPosition(57, 50)
     stopWatch.setPosition(46, 63)
     timer.setPosition(35, 76)
-    acceleration.setPosition(57, 89)
-    dice.setPosition(32, 102)
+    dice.setPosition(32, 89)
     cursorSprite.setPosition(8, 24)
     _return = textsprite.create("'B' to return", 0, 2)
     _return.setPosition(80, 114)
     menuOpen = true
+    optionOpen = false
+    cursorPosition = 0
 }
 function desktop () {
     scene.setBackgroundColor(4)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Text)
     greeting = textsprite.create("Hello Marc!", 0, 8)
     greeting.setPosition(30, 5)
-    temperatureSprite.setText("Temperature: " + temperature + " C")
+    temperatureSprite = textsprite.create("Temperature: " + temperature + "C", 0, 8)
     temperatureSprite.setPosition(58, 95)
-    lightSprite.setText("Light level: " + light2 + " units")
+    lightSprite = textsprite.create("Light level: " + light2 + "units", 0, 8)
     lightSprite.setPosition(70, 105)
     greeting.setVelocity(30, 30)
     temperatureSprite.setVelocity(0, 0)
@@ -109,7 +146,6 @@ function desktop () {
     sprites.destroy(dice)
     sprites.destroy(stopWatch)
     sprites.destroy(timer)
-    sprites.destroy(tilt)
     sprites.destroy(menu)
     sprites.destroy(cursorSprite)
     sprites.destroy(_return)
@@ -151,42 +187,39 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 let _return: TextSprite = null
-let acceleration: TextSprite = null
 let dice: TextSprite = null
 let timer: TextSprite = null
-let stopWatch: TextSprite = null
-let tilt: TextSprite = null
 let menu: TextSprite = null
+let stopWatch: TextSprite = null
+let accelerationZ: TextSprite = null
+let accelerationY: TextSprite = null
+let accelerationX: TextSprite = null
+let acceleration: TextSprite = null
+let light2 = 0
+let lightSprite: TextSprite = null
+let temperature = 0
+let temperatureSprite: TextSprite = null
 let optionOpen = false
 let cursorSprite: TextSprite = null
 let cursorPosition = 0
 let menuOpen = false
-let lightSprite: TextSprite = null
-let temperatureSprite: TextSprite = null
 let greeting: TextSprite = null
-let light2 = 0
-let temperature = 0
 scene.setBackgroundColor(4)
-temperature = controller.temperature(ControllerTemperatureUnit.Celsius)
-light2 = controller.lightLevel()
 greeting = textsprite.create("Hello Marc!", 0, 8)
-temperatureSprite = textsprite.create("Temperature: " + temperature + " C", 0, 8)
-lightSprite = textsprite.create("Light level: " + light2 + " units", 0, 8)
 greeting.setPosition(30, 5)
-temperatureSprite.setPosition(53, 95)
-lightSprite.setPosition(65, 105)
 greeting.setVelocity(30, 30)
-temperatureSprite.setVelocity(0, 0)
-lightSprite.setVelocity(0, 0)
 greeting.setBounceOnWall(true)
-temperatureSprite.setBounceOnWall(true)
-lightSprite.setBounceOnWall(true)
 greeting.setScale(1.5, ScaleAnchor.Middle)
-temperatureSprite.setScale(1, ScaleAnchor.Middle)
-lightSprite.setScale(1, ScaleAnchor.Middle)
 menuOpen = false
 cursorPosition = 0
 game.onUpdateInterval(500, function () {
     temperature = controller.temperature(ControllerTemperatureUnit.Celsius)
     light2 = controller.lightLevel()
+})
+game.onUpdateInterval(500, function () {
+    if (cursorPosition == 2 && optionOpen == true) {
+        accelerationX.setText("X:" + controller.acceleration(ControllerDimension.X))
+        accelerationY.setText("Y:" + controller.acceleration(ControllerDimension.Y))
+        accelerationZ.setText("Z:" + controller.acceleration(ControllerDimension.Z))
+    }
 })
